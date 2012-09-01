eddc864e0b920d53042295cc03a3cbcbf5aadfca
Dominic Rausch <Dominic.Rausch@googlemail.com>
2012-09-01 16:14:06 +0200

    removed tee

--- code/1msparse.sh
+++ code/1msparse.sh
@@ -6,44 +6,44 @@ PLACES="1"
 rm $RESULT
 PLACES="1"
 export X10_NPLACES=$PLACES
-echo -e "\n $PLACES Place "| tee -a $RESULT
-echo -e "serial_list mode"| tee -a $RESULT
-./bfs_start -alg serial_list $GRAPH -benchmark 3|  tee -a $RESULT
-echo -e "serial_sparse mode"| tee -a $RESULT
-./bfs_start -alg serial_sparse $GRAPH -benchmark 3 | tee -a $RESULT
-echo -e "1d_list mode"| tee -a $RESULT
-./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
-echo -e "2d_list_alt mode"| tee -a $RESULT
-./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "\n $PLACES Place "
+echo -e "serial_list mode"
+./bfs_start -alg serial_list $GRAPH -benchmark 3
+echo -e "serial_sparse mode"
+./bfs_start -alg serial_sparse $GRAPH -benchmark 3 
+echo -e "1d_list mode"
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 
+echo -e "2d_list_alt mode"
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 
 
 PLACES="2"
 export X10_NPLACES=$PLACES
-echo -e "\n\n$PLACES Place "| tee -a $RESULT
-echo -e "1d_list mode"| tee -a $RESULT
-./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
-echo -e "2d_list_alt mode"| tee -a $RESULT
-./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "\n\n$PLACES Place "
+echo -e "1d_list mode"
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 
+echo -e "2d_list_alt mode"
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 
 
 PLACES="4"
 export X10_NPLACES=$PLACES
-echo -e "\n\n$PLACES Place "| tee -a $RESULT
-echo -e "1d_list mode"| tee -a $RESULT
-./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
-echo -e "2d_list_alt mode"| tee -a $RESULT
-./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "\n\n$PLACES Place "
+echo -e "1d_list mode"
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 
+echo -e "2d_list_alt mode"
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 
 
 PLACES="6"
 export X10_NPLACES=$PLACES
-echo -e "\n\n$PLACES Place "| tee -a $RESULT
-echo -e "1d_list mode"| tee -a $RESULT
-./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
-echo -e "2d_list_alt mode"| tee -a $RESULT
-./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "\n\n$PLACES Place "
+echo -e "1d_list mode"
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 
+echo -e "2d_list_alt mode"
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 
 
 PLACES="8"
 export X10_NPLACES=$PLACES
-echo -e "\n\n$PLACES Place "| tee -a $RESULT
-echo -e "1d_list mode"| tee -a $RESULT
-./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
-echo -e "2d_list_alt mode"| tee -a $RESULT
-./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "\n\n$PLACES Place "
+echo -e "1d_list mode"
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 
+echo -e "2d_list_alt mode"
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 

5b1c2ff892167d67fd69680ca58f9f02d8749f80
Dominic Rausch <rausch@i44pc25.info.uni-karlsruhe.de>
2012-09-01 14:39:44 +0200

    benchmarking scripts

---
+++ code/10ksparse.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+GRAPH="../../graphs/10ksparse.sgraph"
+RESULT="10ksparse.results"
+PLACES="1"
+
+rm $RESULT
+PLACES="1"
+export X10_NPLACES=$PLACES
+echo -e "\n $PLACES Place "| tee -a $RESULT
+echo -e "serial_list mode"| tee -a $RESULT
+./bfs_start -alg serial_list $GRAPH -benchmark 3|  tee -a $RESULT
+echo -e "serial_sparse mode"| tee -a $RESULT
+./bfs_start -alg serial_sparse $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "1d_list mode"| tee -a $RESULT
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "2d_list_alt mode"| tee -a $RESULT
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+
+PLACES="2"
+export X10_NPLACES=$PLACES
+echo -e "\n\n$PLACES Place "| tee -a $RESULT
+echo -e "1d_list mode"| tee -a $RESULT
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "2d_list_alt mode"| tee -a $RESULT
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+
+PLACES="4"
+export X10_NPLACES=$PLACES
+echo -e "\n\n$PLACES Place "| tee -a $RESULT
+echo -e "1d_list mode"| tee -a $RESULT
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "2d_list_alt mode"| tee -a $RESULT
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+
+PLACES="6"
+export X10_NPLACES=$PLACES
+echo -e "\n\n$PLACES Place "| tee -a $RESULT
+echo -e "1d_list mode"| tee -a $RESULT
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "2d_list_alt mode"| tee -a $RESULT
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
+
+PLACES="8"
+export X10_NPLACES=$PLACES
+echo -e "\n\n$PLACES Place "| tee -a $RESULT
+echo -e "1d_list mode"| tee -a $RESULT
+./bfs_start -alg 1d_list $GRAPH -benchmark 3 | tee -a $RESULT
+echo -e "2d_list_alt mode"| tee -a $RESULT
+./bfs_start -alg 2d_list_alt $GRAPH -benchmark 3 | tee -a $RESULT
